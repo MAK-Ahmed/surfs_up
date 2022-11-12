742ad9c90cf475f3e3d9d64714f988856bc36957
MAK-Ahmed <MAhmed27@northwell.edu>
2022-11-12 14:06:48 -0500

    update folders

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-#from flask import Flask
-#app = Flask(__name__)
-#@app.route('/')
-#def hello_world():
+#1 Import flask
+from flask import Flask
 
-import datetime as dt
-import numpy as np
-import pandas as pd
+#2. create an app, being sure to pass __name__
+app = Flask(__name__)
+
+#3. Define what to do when a user goes to the index route
+@app.route('/')
+
+#4. Define what to do when a user goes to the /about route
+def hello_world():
+
+if __name__=="__main__":
+    app.run(debug=True)
